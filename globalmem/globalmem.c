e0c38c67e5da1f5b6f78b9da498fea93b281521d
asicer <asicer@live.cn>
2014-07-23 17:14:18 +0800

    Update globalmem.c
    some improvement on initial version

--- globalmem/globalmem.c
+++ globalmem/globalmem.c
@@ -13,11 +13,11 @@
 #define GLOBALMEM_SIZE 0x1000
 #define MEM_CLEAR 0x1
 #define GLOBALMEM_MAJOR 225
+#define NOD_CNT 3
 
 #define PRINT_PRIORITY KERN_INFO
 
 #define DEBUG
-#define SEMA
 
 #ifdef DEBUG
 	#define dbg_printk(fmt, args...) printk(PRINT_PRIORITY "<%s: %3d> "fmt, __FUNCTION__, __LINE__, ##args)
@@ -30,9 +30,7 @@ static int globalmem_major = GLOBALMEM_MAJOR;
 struct globalmem_dev {
 	struct cdev cdev;
 	unsigned char mem[GLOBALMEM_SIZE];
-#ifdef SEMA
 	struct semaphore sem;
-#endif
 };
 
 struct globalmem_dev *globalmem_devp;
@@ -63,15 +61,8 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t count,
 	if (count > GLOBALMEM_SIZE - p)
 		count = GLOBALMEM_SIZE - p;
 	
-#ifdef SEMA
 	if (down_interruptible(&dev->sem))
 		return - ERESTARTSYS;
-#endif
-
-#if 0
-	if (count == 0)
-		return;
-#endif
 
 	dbg_printk("count: %u, p: %lu\n", count, p);
 
@@ -84,9 +75,7 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t count,
 		dbg_printk("read %u bytes(s) from %lu\n", count, p);
 	}
 
-#ifdef SEMA
 	up(&dev->sem);
-#endif
 
 	return ret;
 }
@@ -99,17 +88,13 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t
 
 	dbg_printk("%u, %lu\n", count, p);
 
-#ifdef SEMA
 	if (down_interruptible(&dev->sem))
 		return - ERESTARTSYS;
-#endif
 
 	if (p > GLOBALMEM_SIZE)
 		return 0;
 	if (count > GLOBALMEM_SIZE - p)
 		count = GLOBALMEM_SIZE - p;
-
-	//memset(dev->mem, 0, GLOBALMEM_SIZE);
 
 	if (copy_from_user(dev->mem + p, buf, count))
 		ret = - EFAULT;
@@ -119,11 +104,9 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t
 		ret = count;
 		dbg_printk("write %u bytes(s) from %lu\n", count, p);
 	}
-
-#ifdef SEMA
+	
 	up(&dev->sem);
-#endif
-
+	
 	return ret;
 }
 
@@ -134,16 +117,10 @@ static long globalmem_ioctl(struct file *filp, unsigned int cmd, unsigned long a
 	switch (cmd)
 	{
 		case MEM_CLEAR:
-#ifdef SEMA
 			if (down_interruptible(&dev->sem))
-			return - ERESTARTSYS;
-#endif
-
+				return - ERESTARTSYS;
 			memset(dev->mem, 0, GLOBALMEM_SIZE);
-
-#ifdef SEMA
 			up(&dev->sem);
-#endif
 			break;
 		default:
 			return - EINVAL;
@@ -162,16 +139,13 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 				ret = - EINVAL;
 				break;
 			}
-			
 			if ((unsigned int)offset > GLOBALMEM_SIZE)
 			{
 				ret = - EINVAL;
 				break;
 			}
-
 			filp->f_pos += offset;
 			ret = filp->f_pos;
-			
 			break;
 		case 1:	//offset relative to file current position
 			if ((filp->f_pos + offset) < 0)
@@ -179,16 +153,13 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 				ret = - EINVAL;
 				break;
 			}
-			
 			if ((filp->f_pos + offset) > GLOBALMEM_SIZE)
 			{
 				ret = - EINVAL;
 				break;
 			}
-			
 			filp->f_pos += offset;
 			ret = filp->f_pos;
-			
 			break;
 		default:
 			ret = - EINVAL;
@@ -221,54 +192,50 @@ static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
 
 int globalmem_init(void)
 {
-	int result;
+	int i, result;
 	dev_t devno = MKDEV(globalmem_major, 0);
 
 	if (globalmem_major)
-		result = register_chrdev_region(devno, 3, "globalmem");
+		result = register_chrdev_region(devno, NOD_CNT, "globalmem");
 	else
 	{
-		result = alloc_chrdev_region(&devno, 0, 3, "globalmem");
+		result = alloc_chrdev_region(&devno, 0, NOD_CNT, "globalmem");
 		globalmem_major = MAJOR(devno);
 	}
 	if (result < 0)
 		return result;
 	
-	globalmem_devp = kmalloc(3*sizeof(struct globalmem_dev), GFP_KERNEL);
+	globalmem_devp = kmalloc(NOD_CNT * sizeof(struct globalmem_dev), GFP_KERNEL);
 	if (!globalmem_devp)
 	{
 		result = - ENOMEM;
 		goto fail_malloc;
 	}
 
-	memset(globalmem_devp, 0, 3*sizeof(struct globalmem_dev));
+	memset(globalmem_devp, 0, NOD_CNT * sizeof(struct globalmem_dev));
 
-	globalmem_setup_cdev(&globalmem_devp[0], 0);
-	globalmem_setup_cdev(&globalmem_devp[1], 1);
-	globalmem_setup_cdev(&globalmem_devp[2], 2);
-
-#ifdef SEMA
-	sema_init(&globalmem_devp[0].sem, 1);
-	sema_init(&globalmem_devp[1].sem, 1);
-	sema_init(&globalmem_devp[2].sem, 1);
-#endif
+	for (i = 0, i < NOD_CNT, i++)
+	{
+		globalmem_setup_cdev(&globalmem_devp[i], i);
+		sema_init(&globalmem_devp[i].sem, 1);
+	}
 
 	dbg_printk("globalmem init\n");
 
 	return 0;
 
 fail_malloc:
-	unregister_chrdev_region(devno, 1);
+	unregister_chrdev_region(devno, 3);
 	return result;
 }
 
 void globalmem_exit(void)
 {
-	cdev_del(&(globalmem_devp[0].cdev));
-	cdev_del(&(globalmem_devp[1].cdev));
-	cdev_del(&(globalmem_devp[2].cdev));
+	int i;
+	for (i = 0; i < NOD_CNT, i++)
+		cdev_del(&(globalmem_devp[i].cdev));
 	kfree(globalmem_devp);
-	unregister_chrdev_region(MKDEV(globalmem_major, 0), 3);
+	unregister_chrdev_region(MKDEV(globalmem_major, 0), NOD_CNT);
 	dbg_printk("globalmem exit\n");
 }
 
